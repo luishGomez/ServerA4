32c3ac6755075ffdb556fcc38fc6e0c6a88c1c33
2dam <2dam@MACINF02.TartangaLH.eus>
2019-12-03 11:32:08 +0100

    usuario oferta

--- src/java/entity/Oferta.java
+++ src/java/entity/Oferta.java
@@ -5,10 +5,105 @@
  */
 package entity;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 /**
  *
- * @author 2dam
+ * @author sergio
  */
-public class Oferta {
+@Entity
+@Table(name = "oferta", schema = "serverA4DB")
+public class Oferta implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @NotNull private Integer idOferta;
+    @NotNull private String titulo;
+    @NotNull private Timestamp fechaInicio;
+    @NotNull private Timestamp fechaFin;
+    @ManyToMany
+    @JoinTable(name = "oferta_pack", schema = "serverA4DB")
+    private Set<Pack> packs;
+
+    /**
+     * @return the idOferta
+     */
+    public Integer getIdOferta() {
+        return idOferta;
+    }
+
+    /**
+     * @param idOferta the idOferta to set
+     */
+    public void setIdOferta(Integer idOferta) {
+        this.idOferta = idOferta;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Timestamp getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Timestamp fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Timestamp getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Timestamp fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the packs
+     */
+    public Set<Pack> getPacks() {
+        return packs;
+    }
+
+    /**
+     * @param packs the packs to set
+     */
+    public void setPacks(Set<Pack> packs) {
+        this.packs = packs;
+    }
     
 }

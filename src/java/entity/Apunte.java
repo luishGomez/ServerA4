db48c2e38d6087227b27aea8a78548a206bd4af2
2dam <2dam@HZ341352.TartangaLH.eus>
2019-12-04 09:23:04 +0100

    Clases apunte y cliente & setters and getters

--- src/java/entity/Apunte.java
+++ src/java/entity/Apunte.java
@@ -55,5 +55,189 @@ public class Apunte implements Serializable {
     @NotNull
     @ManyToOne
     private Materia materia;
+
+    /**
+     * @return the idApunte
+     */
+    public Integer getIdApunte() {
+        return idApunte;
+    }
+
+    /**
+     * @param idApunte the idApunte to set
+     */
+    public void setIdApunte(Integer idApunte) {
+        this.idApunte = idApunte;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the archivo
+     */
+    public Blob getArchivo() {
+        return archivo;
+    }
+
+    /**
+     * @param archivo the archivo to set
+     */
+    public void setArchivo(Blob archivo) {
+        this.archivo = archivo;
+    }
+
+    /**
+     * @return the fechaValidacion
+     */
+    public Date getFechaValidacion() {
+        return fechaValidacion;
+    }
+
+    /**
+     * @param fechaValidacion the fechaValidacion to set
+     */
+    public void setFechaValidacion(Date fechaValidacion) {
+        this.fechaValidacion = fechaValidacion;
+    }
+
+    /**
+     * @return the likeCont
+     */
+    public int getLikeCont() {
+        return likeCont;
+    }
+
+    /**
+     * @param likeCont the likeCont to set
+     */
+    public void setLikeCont(int likeCont) {
+        this.likeCont = likeCont;
+    }
+
+    /**
+     * @return the dislikeCont
+     */
+    public int getDislikeCont() {
+        return dislikeCont;
+    }
+
+    /**
+     * @param dislikeCont the dislikeCont to set
+     */
+    public void setDislikeCont(int dislikeCont) {
+        this.dislikeCont = dislikeCont;
+    }
+
+    /**
+     * @return the votantes
+     */
+    public Set <Cliente> getVotantes() {
+        return votantes;
+    }
+
+    /**
+     * @param votantes the votantes to set
+     */
+    public void setVotantes(Set <Cliente> votantes) {
+        this.votantes = votantes;
+    }
+
+    /**
+     * @return the precio
+     */
+    public float getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the creador
+     */
+    public Cliente getCreador() {
+        return creador;
+    }
+
+    /**
+     * @param creador the creador to set
+     */
+    public void setCreador(Cliente creador) {
+        this.creador = creador;
+    }
+
+    /**
+     * @return the compras
+     */
+    public Set <Compra> getCompras() {
+        return compras;
+    }
+
+    /**
+     * @param compras the compras to set
+     */
+    public void setCompras(Set <Compra> compras) {
+        this.compras = compras;
+    }
+
+    /**
+     * @return the packs
+     */
+    public Set <Pack> getPacks() {
+        return packs;
+    }
+
+    /**
+     * @param packs the packs to set
+     */
+    public void setPacks(Set <Pack> packs) {
+        this.packs = packs;
+    }
+
+    /**
+     * @return the materia
+     */
+    public Materia getMateria() {
+        return materia;
+    }
+
+    /**
+     * @param materia the materia to set
+     */
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+    
+    
     
 }
--- src/java/entity/Cliente.java
+++ src/java/entity/Cliente.java
@@ -8,8 +8,10 @@ package entity;
 import java.io.Serializable;
 import java.sql.Blob;
 import java.util.Set;
+import static javax.persistence.CascadeType.ALL;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -21,9 +23,67 @@ import javax.persistence.Table;
 public class Cliente extends User implements Serializable{
     private static final long serialVersionUID=1L;
     
-    
+    @OneToMany(cascade=ALL,mappedBy="propietario")
     private Set <Compra> compras;
     private Set <Apunte> apuntes;
     private float saldo;
     private Blob foto;
+
+    /**
+     * @return the compras
+     */
+    public Set <Compra> getCompras() {
+        return compras;
+    }
+
+    /**
+     * @param compras the compras to set
+     */
+    public void setCompras(Set <Compra> compras) {
+        this.compras = compras;
+    }
+
+    /**
+     * @return the apuntes
+     */
+    public Set <Apunte> getApuntes() {
+        return apuntes;
+    }
+
+    /**
+     * @param apuntes the apuntes to set
+     */
+    public void setApuntes(Set <Apunte> apuntes) {
+        this.apuntes = apuntes;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public float getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the foto
+     */
+    public Blob getFoto() {
+        return foto;
+    }
+
+    /**
+     * @param foto the foto to set
+     */
+    public void setFoto(Blob foto) {
+        this.foto = foto;
+    }
+    
+    
 }
